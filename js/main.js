2288f2f191bc403b2de6b554ff6c1c0b58f63ed9
doriquinnn <doriquinn@snu.ac.kr>
2023-08-19 18:30:08 -0400

    Added About Me Area

--- js/main.js
+++ js/main.js
@@ -23,4 +23,21 @@ $(document).ready(function(){
         gallery:{enabled:true}
       });
 
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop:true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            544: {
+                items: 2
+            }
+        }
+
+    })
+
 });
